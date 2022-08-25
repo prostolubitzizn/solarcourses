16663d47e7f52d739310ce280a9c5f685bbacca1
Natasha <valchishina02@mail.ru>
2022-08-25 13:38:33 +0300

    website alpha

---
+++ SolarTest/Website/Controllers/BirthdayController.cs
@@ -0,0 +1,79 @@
+using System.Collections.Generic;
+using Common.Models;
+using Common.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Website.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BirthdayController : ControllerBase
+    {
+
+        private readonly IBirthdayService _birthdayService;
+
+        public BirthdayController(IBirthdayService birthdayService)
+        {
+            _birthdayService = birthdayService;
+        }
+
+        [HttpGet] 
+        [Route("allBirthdays")]
+        public IEnumerable<Birthday> GetAll()
+        {
+            return _birthdayService.GetBirthdays();
+        }
+
+        [HttpGet]
+        [Route("todayBirthday")]
+        public IEnumerable<Birthday> GetTodayBirthdays()
+        {
+            return _birthdayService.GetTodaysBirthdays();
+        }
+
+        [HttpGet]
+        [Route("soonBirthday")]
+        public IEnumerable<Birthday> GetSoonBirthdays()
+        {
+            return _birthdayService.GetSoonBirthdays();
+        }
+
+        [HttpGet]
+        [Route("outdatedBirthday")]
+        public IEnumerable<Birthday> GetOutdatedBirthdays()
+        {
+            return _birthdayService.GetOutDatedBirthdays();
+        }
+        
+
+        [HttpGet]
+        [Route("getBirthday/{id}")]
+        public Birthday GetUser(int id)
+        {
+            return _birthdayService.Get(id);
+        }
+
+        [HttpPut]
+        [Route("insertBirthday")]
+        public int? Insert(Birthday birthday)
+        {
+            return _birthdayService.Create(birthday);
+        }
+
+        [HttpPut]
+        [Route("updateBirthday")]
+        public bool Update(Birthday birthday)
+        {
+            _birthdayService.Update(birthday);
+            return true;
+        }
+
+        [HttpDelete]
+        [Route("deleteBirthday")]
+        public bool Delete(int id)
+        {
+            _birthdayService.Delete(id);
+            return true;
+        }
+    }
+}
---
+++ SolarTest/Website/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+console.log("Hello world!");
+
+$.get( "https://localhost:5002/Birthday/allBirthdays", function( data ) {
+    console.log("data", data);
+    alert( "Load was performed." );
+});
